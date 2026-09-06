1685ab8f9abfb28ad36cf156af823227b4c314d8
alo986761986-gif <alo986761986@gmail.com>
2026-09-06 11:41:50 +0200

    chore: configure Next.js

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+import type { NextConfig } from 'next';
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  experimental: { optimizePackageImports: ['zod'] },
+};
+export default nextConfig;
